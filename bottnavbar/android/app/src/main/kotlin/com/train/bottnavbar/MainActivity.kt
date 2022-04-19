46efc627157f8c014a120e88d5e2cb26d122a57d
Idrussalam <100740135+Idrussalam@users.noreply.github.com>
2022-04-19 21:10:01 +0700

    Tugas Pem Mobile

---
+++ bottnavbar/android/app/src/main/kotlin/com/train/bottnavbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.train.bottnavbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gridview/android/app/src/main/kotlin/com/train/gridview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.train.gridview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
